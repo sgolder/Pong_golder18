f53d7d094e0bd85aeae45114b681549bbbdcfbab
golder18 <golder18@up.edu>
2018-03-23 10:08:02 -0700

    Cleaner commit

--- app/src/main/java/com/example/toshiba/pong_golder18/PongAnimator.java
+++ app/src/main/java/com/example/toshiba/pong_golder18/PongAnimator.java
@@ -41,49 +41,8 @@ public class PongAnimator implements Animator {
     private int width = 1536; // width of our surface
     private int height = 2000; // height of our surface
 
-    private boolean betweenPlay = false;
+    private boolean betweenGames = false;
 
-
-    /**
-     * Interval between animation frames: .03 seconds (i.e., about 33 times
-     * per second).
-     *
-     * @return the time interval between frames, in milliseconds.
-     */
-    @Override
-    public int interval() {
-        return 30;
-    }
-
-    /**
-     * The background color: a light blue.
-     *
-     * @return the background color onto which we will draw the image.
-     */
-    @Override
-    public int backgroundColor() {
-        return Color.rgb(203, 247, 237);
-    }
-
-    @Override
-    public boolean doPause() {
-        if( betweenPlay ) {
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean doQuit() {
-        return false;
-    }
-
-    /**
-     * Paints ball, walls and paddle on clock tick
-     * Keeps ball from going outside the walls
-     *
-     * @param g the graphics object on which to draw
-     */
     @Override
     public void tick(Canvas g) {
         // bump our x and y counts either up or down by one,
@@ -116,6 +75,68 @@ public class PongAnimator implements Animator {
                 newBall();
             }
         }
+        duringGameView(g);
+
+        if( ballY+50 > 1800 ) { // If ball reaches the paddle height
+            // Hits the paddle, reverse as if hitting a wall
+            if ( ballX+50 > paddleLeft && ballX-50 < paddleRight ) {
+                reverseY = !reverseY;
+            }
+            // Misses the paddle
+            else if ( ballY < 2000 ) {
+                betweenGameView(g);
+            }
+        }
+    }
+
+    /**
+     * Adjusts game based on user choice for difficulty, restarting
+     * game play, or moving the paddle
+     *
+     */
+    @Override
+    public void onTouch(MotionEvent event) {
+        // Where the user touched
+        float eventX = event.getX();
+        float eventY = event.getY();
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if(betweenGames) {
+                if( eventY > 100 && eventY < 200 ) {
+                    // Set paddle size based on difficulty choice
+                    if( eventX > 125 && eventX < 525 ) {      // easy
+                        paddleSize = 200;
+                    }
+                    else if( eventX > 575 && eventX < 975 ) { // med
+                        paddleSize = 100;
+                    }
+                    else if( eventX > 1025 && eventX < 1425) { // dif
+                        paddleSize = 25;
+                    }
+                    else { // not on a button
+                        newBall();
+                        betweenGames = false;
+                    }
+                }
+                // Somewhere else on the screen
+                else {
+                    newBall();
+                    betweenGames = false;
+                }
+            }
+        }
+        // User is moving the paddle
+        else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            paddleLeft = event.getX()-paddleSize;
+            paddleRight = event.getX()+paddleSize;
+        }
+        else if (event.getAction() == MotionEvent.ACTION_UP) {
+            paddleLeft = event.getX()-paddleSize;
+            paddleRight = event.getX()+paddleSize;
+        }
+    }
+
+    public void duringGameView(Canvas g) {
         // Draw walls
         Paint wall = new Paint();
         wall.setColor(Color.BLACK);
@@ -136,98 +157,44 @@ public class PongAnimator implements Animator {
         paddlePaint.setColor(Color.BLACK);
         g.drawRect(paddleLeft, 1800, paddleRight, 1825, paddlePaint);
         paddlePaint.setColor(0xff0000ff);
-
-        if( ballY+50 > 1800 ) { // If ball reaches the paddle height
-            // Hits the paddle, reverse as if hitting a wall
-            if ( ballX+50 > paddleLeft && ballX-50 < paddleRight ) {
-                reverseY = !reverseY;
-            }
-            // Misses the paddle
-            else if ( ballY < 2000 ) {
-                /**
-                 External Citation
-                 Date: 3/20/2018
-                 Problem: Couldn't remember how to change font size
-                 Resource:
-                 https://developer.android.com/reference/android/
-                 graphics/Paint.html
-                 Solution: I used the setTextSize method
-                 */
-
-                // Tell user to tap screen to continue playing
-                Paint restart = new Paint();
-                restart.setColor(Color.BLACK);
-                restart.setTextSize(50.0f);
-                g.drawText("Tap screen to begin with new ball",
-                        400, 700, restart);
-                betweenPlay = true;
-
-                // Draw difficulty mode buttons
-                Paint easyMode = new Paint(); // Easy
-                easyMode.setColor(Color.rgb(142, 168, 195));
-                g.drawRect(125, 100, 525, 200, easyMode);
-                Paint mediumMode = new Paint(); // Medium
-                mediumMode.setColor(Color.rgb( 64, 110, 142));
-                g.drawRect(575, 100, 975, 200, mediumMode);
-                Paint hardMode = new Paint(); // Hard
-                hardMode.setColor(Color.rgb( 35, 57, 91));
-                g.drawRect(1025, 100, 1425, 200, hardMode);
-                // Write text to go on buttons
-                Paint modeText = new Paint();
-                modeText.setColor(Color.WHITE);
-                modeText.setTextSize(50.0f);
-                g.drawText("Easy", 270, 170, modeText);
-                g.drawText("Medium", 690, 170, modeText);
-                g.drawText("Hard", 1175, 170, modeText);
-            }
-        }
     }
 
-    /**
-     * Adjusts game based on user choice for difficulty, restarting
-     * game play, or moving the paddle
-     *
-     */
-    @Override
-    public void onTouch(MotionEvent event) {
-        // Where the user touched
-        float eventX = event.getX();
-        float eventY = event.getY();
+    public void betweenGameView(Canvas g) {
+        /**
+         External Citation
+         Date: 3/20/2018
+         Problem: Couldn't remember how to change font size
+         Resource:
+         https://developer.android.com/reference/android/
+         graphics/Paint.html
+         Solution: I used the setTextSize method
+         */
 
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            if( betweenPlay ) {
-                if( eventY > 100 && eventY < 200 ) {
-                    // Set paddle size based on difficulty choice
-                    if( eventX > 125 && eventX < 525 ) {      // easy
-                        paddleSize = 200;
-                    }
-                    else if( eventX > 575 && eventX < 975 ) { // med
-                        paddleSize = 100;
-                    }
-                    else if( eventX > 1025 && eventX < 1425) { // dif
-                        paddleSize = 25;
-                    }
-                    else { // not on a button
-                        newBall();
-                        betweenPlay = false;
-                    }
-                }
-                // Somewhere else on the screen
-                else {
-                    newBall();
-                    betweenPlay = false;
-                }
-            }
-        }
-        // User is moving the paddle
-        else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            paddleLeft = event.getX()-paddleSize;
-            paddleRight = event.getX()+paddleSize;
-        }
-        else if (event.getAction() == MotionEvent.ACTION_UP) {
-            paddleLeft = event.getX()-paddleSize;
-            paddleRight = event.getX()+paddleSize;
-        }
+        // Tell user to tap screen to continue playing
+        Paint restart = new Paint();
+        restart.setColor(Color.BLACK);
+        restart.setTextSize(50.0f);
+        g.drawText("Tap screen to begin with new ball",
+                400, 700, restart);
+        betweenGames = true;
+
+        // Draw difficulty mode buttons
+        Paint easyMode = new Paint(); // Easy
+        easyMode.setColor(Color.rgb(142, 168, 195));
+        g.drawRect(125, 100, 525, 200, easyMode);
+        Paint mediumMode = new Paint(); // Medium
+        mediumMode.setColor(Color.rgb( 64, 110, 142));
+        g.drawRect(575, 100, 975, 200, mediumMode);
+        Paint hardMode = new Paint(); // Hard
+        hardMode.setColor(Color.rgb( 35, 57, 91));
+        g.drawRect(1025, 100, 1425, 200, hardMode);
+        // Write text to go on buttons
+        Paint modeText = new Paint();
+        modeText.setColor(Color.WHITE);
+        modeText.setTextSize(50.0f);
+        g.drawText("Easy", 270, 170, modeText);
+        g.drawText("Medium", 690, 170, modeText);
+        g.drawText("Hard", 1175, 170, modeText);
     }
 
     /**
@@ -245,5 +212,45 @@ public class PongAnimator implements Animator {
         speedY = rand.nextInt(50)+5;
     }
 
+    /**
+     * Interval between animation frames: .03 seconds (i.e., about 33 times
+     * per second).
+     *
+     * @return the time interval between frames, in milliseconds.
+     */
+    @Override
+    public int interval() {
+        return 30;
+    }
+
+    /**
+     * The background color: a light blue.
+     *
+     * @return the background color onto which we will draw the image.
+     */
+    @Override
+    public int backgroundColor() {
+        return Color.rgb(203, 247, 237);
+    }
+
+    @Override
+    public boolean doPause() {
+        if(betweenGames) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean doQuit() {
+        return false;
+    }
+
+    /**
+     * Paints ball, walls and paddle on clock tick
+     * Keeps ball from going outside the walls
+     *
+     * @param g the graphics object on which to draw
+     */
 }
 
