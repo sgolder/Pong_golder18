77246336d3a34cc690ae44609608b36240236cb6
sgolder <35584062+sgolder@users.noreply.github.com>
2018-03-30 00:00:48 -0700

    Update PongAnimator.java

--- app/src/main/java/com/example/toshiba/pong_golder18/PongAnimator.java
+++ app/src/main/java/com/example/toshiba/pong_golder18/PongAnimator.java
@@ -3,16 +3,18 @@ package com.example.toshiba.pong_golder18;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.MotionEvent;
 
 import java.util.Random;
 /**
  * Class that animates the ball to bounce within a field
- * If the balls gets to the bottom edge of the field without
- * being bounced back by the user moving the paddle, the game
- * restarts.
+ * If the balls gets to the bottom or top edge of the field without
+ * being bounced back by the user moving the paddle or the computer,
+ * the game restarts. Points are gained when the opponent misses.
  *
  * Playing difficulty is determined by 3 sizes of the paddle
+ * and the chances of the computer opponent missing the ball.
  *
  * @author Sarah Golder
  * @version March 2018
@@ -32,7 +34,9 @@ public class PongAnimator implements Animator {
     // Properties for computer player
     private float compPaddleLeft = 500;
     private float compPaddleRight = 700;
-    private int chanceOfMissing = 6;
+    private int chanceOfMissing = 2;
+    private float trackingX;
+    private boolean willMiss = false;
 
     // Properties of the ball
     private boolean reverseX = false;
@@ -51,8 +55,12 @@ public class PongAnimator implements Animator {
 
     private boolean betweenGames = false;
 
-    //TODO: create an initialization method?
-
+    /**
+     * Move ball across screen according to logical ticks
+     * and moves computer paddle to block and occasionally miss
+     * the ball
+     *
+     */
     @Override
     public void tick(Canvas g) {
         // bump our x and y counts either up or down by one,
@@ -85,40 +93,48 @@ public class PongAnimator implements Animator {
                 newBall();
             }
         }
-        duringGameView(g);
 
         // If ball reaches the player paddle height
         if( ballY+50 > 1800 ) { // Player paddle
-            // Hits the paddle, reverse as if hitting a wall
-            if ( ballX+50 > playerPaddleLeft && ballX-50 < playerPaddleRight ) {
-                playerScore++;
+            // Hits the paddle
+            if ( ballX+50 > playerPaddleLeft &&
+                    ballX-50 < playerPaddleRight ) {
                 reverseY = !reverseY;
             }
             // Misses player's paddle
             else if ( ballY < 2000 ) {
-                playerScore =- 5;
+                computerScore++;
                 betweenGameView(g);
             }
         }
-        else if( ballY-50 < 200 ) { // Computer paddle
-            // Hits computer paddle, reverse as if hitting a wall
-            if ( ballX+50 > compPaddleLeft && ballX-50 < compPaddleRight ) {
-                computerScore++;
+        else if( ballY-50 < 100 ) { // Computer paddle
+            // Hits computer paddle
+            if ( ballX+50 > compPaddleLeft &&
+                    ballX-50 < compPaddleRight ) {
                 reverseY = !reverseY;
+                // Randomly choose if computer will miss next turn
+                // based on the difficulty level chosen by user
+                Random rand = new Random();
+                int toMiss = rand.nextInt(chanceOfMissing);
+                if( toMiss == 1 ) {
+                    willMiss = true;
+                }
             }
             // Misses computer paddle
             else if ( ballY < 100 ) {
-                computerScore =- 5;
+                playerScore++;
                 betweenGameView(g);
             }
         }
 
-        // Computer moving paddle
-        Random rand = new Random();
-        int willMiss = rand.nextInt(chanceOfMissing);
-
-        compPaddleLeft = ballX-paddleSize;
-        compPaddleRight = ballX+paddleSize;
+        // Make computer paddle track or lock
+        if( !willMiss && ballY < 800) {
+            trackingX = ballX;
+        }
+        else if ( ballY < 300 ) {
+            trackingX = 400;
+        }
+        duringGameView(g);
     }
 
     /**
@@ -133,28 +149,25 @@ public class PongAnimator implements Animator {
         float eventY = event.getY();
 
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if(eventX > 500 && eventX < 1050 && betweenGames) {
+                //
+                if( eventY > 600 && eventY < 750 ) {        // EASY
+                    paddleSize = 200;
+                    chanceOfMissing = 4;
+                }
+                else if( eventY > 850 && eventY < 1000 ) {  // MEDIUM
+                    paddleSize = 100;
+                    chanceOfMissing = 6;
+                }
+                else if( eventY > 1100 && eventY < 1250 ) { // HARD
+                    paddleSize = 25;
+                    chanceOfMissing = 10;
+                }
+            }
+            // Restart the game
             if(betweenGames) {
-                if( eventY > 100 && eventY < 200 ) {
-                    // Set paddle size based on difficulty choice
-                    if( eventX > 125 && eventX < 525 ) {      // easy
-                        paddleSize = 200;
-                    }
-                    else if( eventX > 575 && eventX < 975 ) { // med
-                        paddleSize = 100;
-                    }
-                    else if( eventX > 1025 && eventX < 1425) { // dif
-                        paddleSize = 25;
-                    }
-                    else { // not on a button
-                        newBall();
-                        betweenGames = false;
-                    }
-                }
-                // Somewhere else on the screen
-                else {
-                    newBall();
-                    betweenGames = false;
-                }
+                newBall();
+                betweenGames = false;
             }
         }
         // User is moving the paddle
@@ -168,11 +181,25 @@ public class PongAnimator implements Animator {
         }
     }
 
+    /**
+     * Paints objects to animator that are relevant to gameplay
+     *
+     */
     public void duringGameView(Canvas g) {
+        /**
+         External Citation
+         Date: 3/26/2018
+         Problem: Didn't know how to add shadows to all the objects
+         Resource:
+         https: https://developer.android.com/reference/android/
+         graphics/Paint.html
+         Solution: I used the setShadowLayer method
+         */
         // Draw walls
         Paint wall = new Paint();
         wall.setColor(Color.WHITE);
         wall.setStrokeWidth(10.0f);
+        wall.setShadowLayer(2, 2, 2, Color.rgb(185, 183, 153));
         g.drawLine(50, 50, 50, 1850, wall);         // left
         g.drawLine(50, 50, 1500, 50, wall);         // top
         g.drawLine(1500, 50, 1500, 1850, wall);     // right
@@ -181,32 +208,28 @@ public class PongAnimator implements Animator {
         // Draw the ball in the correct position.
         Paint ballPaint = new Paint();
         ballPaint.setColor(Color.WHITE);
+        ballPaint.setShadowLayer(2, 2, 2, Color.rgb(185, 183, 153));
         g.drawCircle(ballX, ballY, 50, ballPaint);
         ballPaint.setColor(0xff0000ff);
 
         // Draw player's paddle
         Paint paddlePaint = new Paint();
         paddlePaint.setColor(Color.WHITE);
+        paddlePaint.setShadowLayer(2, 2, 2, Color.rgb(185, 183, 153));
         g.drawRect(playerPaddleLeft, 1800, playerPaddleRight, 1825, paddlePaint);
-        paddlePaint.setColor(0xff0000ff);
 
         // Draw computer's paddle
-        paddlePaint.setColor(Color.WHITE);
+        compPaddleLeft = trackingX-paddleSize;
+        compPaddleRight = trackingX+paddleSize;
         g.drawRect(compPaddleLeft, 75, compPaddleRight, 100, paddlePaint);
         paddlePaint.setColor(0xff0000ff);
-
-        // Draw score card
-        Paint scorePaint = new Paint();
-        scorePaint.setColor(Color.WHITE);
-        scorePaint.setTextSize(50.0f);
-        /*
-        g.drawText("Your", 100, 900, scorePaint );
-        g.drawText("Score: "+playerScore, 100, 950, scorePaint );
-        g.drawText("Computer's", 1200, 900, scorePaint );
-        g.drawText("Score: "+playerScore, 1200, 950, scorePaint );
-        */
     }
 
+    /**
+     * Paints buttons and text to animator for between game
+     * interface
+     *
+     */
     public void betweenGameView(Canvas g) {
         /**
          External Citation
@@ -218,33 +241,49 @@ public class PongAnimator implements Animator {
          Solution: I used the setTextSize method
          */
 
-        // Tell user to tap screen to continue playing
+        // Tell user to select mode and restart
         Paint restart = new Paint();
-        restart.setColor(Color.BLACK);
-        restart.setTextSize(50.0f);
-        //g.drawText("Select difficulty and tap screen to restart",
-        //        400, 400, restart);
-        betweenGames = true;
+        restart.setColor(Color.WHITE);
+        restart.setTextSize(30.0f);
+        restart.setShadowLayer(2, 2, 2, Color.rgb(185, 183, 153));
+        g.drawText("C H O O S E  D I F F I C U L T Y /",
+                560, 450, restart);
+        g.drawText("T A P  S C R E E N  T O  R E S T A R T",
+                535, 500, restart);
 
         // Draw difficulty mode buttons
         Paint modeText = new Paint(); // Text for buttons
         modeText.setColor(Color.WHITE);
         modeText.setTextSize(70.0f);
+        modeText.setShadowLayer(2, 2, 2, Color.rgb(185, 183, 153));
 
         Paint easyMode = new Paint(); // Easy
         easyMode.setColor(Color.rgb(229, 183, 139));
+        easyMode.setShadowLayer(2, 2, 2, Color.rgb(185, 183, 153));
         g.drawRect(500, 600, 1050, 750, easyMode);
         g.drawText("E A S Y", 670, 700, modeText);
 
         Paint mediumMode = new Paint(); // Medium
         mediumMode.setColor(Color.rgb(221, 141, 117));
+        mediumMode.setShadowLayer(2, 2, 2, Color.rgb(185, 183, 153));
         g.drawRect(500, 850, 1050, 1000, mediumMode);
         g.drawText("M E D I U M", 600, 950, modeText);
 
         Paint hardMode = new Paint(); // Hard
         hardMode.setColor(Color.rgb( 138, 113, 106));
+        hardMode.setShadowLayer(2, 2, 2, Color.rgb(185, 183, 153));
         g.drawRect(500, 1100, 1050, 1250, hardMode);
         g.drawText("H A R D", 665, 1200, modeText);
+
+        // Draw score card
+        Paint scorePaint = new Paint();
+        scorePaint.setColor(Color.WHITE);
+        scorePaint.setTextSize(30.0f);
+        scorePaint.setShadowLayer(2, 2, 2, Color.rgb(185, 183, 153));
+        g.drawText("C O M P U T E R ' S  S C O R E: " +
+                computerScore, 570, 1380, scorePaint );
+        g.drawText("Y O U R  S C O R E: " + playerScore, 645, 1430,
+                scorePaint );
     }
 
     /**
@@ -253,13 +292,13 @@ public class PongAnimator implements Animator {
      */
     public void newBall() {
         Random rand = new Random();
-        //countX = rand.nextInt(width-50)+50;
         countX = 150;
         countY = 150;
         reverseX = rand.nextBoolean();
         reverseY = true;
         speedX = rand.nextInt(50)+5;
         speedY = rand.nextInt(50)+5;
+        willMiss = false;
     }
 
     /**
@@ -274,7 +313,7 @@ public class PongAnimator implements Animator {
     }
 
     /**
-     * The background color: a light blue.
+     * The background color: slate.
      *
      * @return the background color onto which we will draw the image.
      */
@@ -296,11 +335,5 @@ public class PongAnimator implements Animator {
         return false;
     }
 
-    /**
-     * Paints ball, walls and paddle on clock tick
-     * Keeps ball from going outside the walls
-     *
-     * @param g the graphics object on which to draw
-     */
 }
 
